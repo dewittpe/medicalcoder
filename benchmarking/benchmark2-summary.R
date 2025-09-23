f6bbe909ecf99bbec76d1e6d1b0c6dfb9df0845d
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-09-23 09:07:11 -0600

    improve benchmark workflow

--- benchmarking/benchmark2-summary.R
+++ benchmarking/benchmark2-summary.R
@@ -1,28 +1,28 @@
 library(data.table)
 library(ggplot2)
 
-bench1 <-
-  list.files("bench1_results", full.names = TRUE) |>
+bench2 <-
+  list.files("bench2_results", full.names = TRUE) |>
   lapply(readRDS) |>
   lapply(setDT) |>
   rbindlist()
 
-bench1[, data_class := fcase(data_class == "DF", "data.frame",
+bench2[, data_class := fcase(data_class == "DF", "data.frame",
                              data_class == "DT", "data.table",
                              data_class == "TBL", "tibble")]
 
 cclr <- c("data.table" = "#8da0cb", "tibble" = "#fc8d62", "data.frame" = "#66c2a5")
 ctyp <- c("data.frame" = 2, "data.table" = 1, "tibble" = 3)
 
-bench1_summary <-
-  bench1[,
+bench2_summary <-
+  bench2[,
          .(mean = mean(time_seconds), median = median(time_seconds), q3 = quantile(time_seconds, prob = 0.75), q1 = quantile(time_seconds, prob = 0.25))
-         , by = .(data_class, subjects, encounters, method, subconditions, flag.method)
+         , by = .(data_class, encounters, method, subconditions, flag.method)
          ]
 
-ggplot(bench1_summary) +
+ggplot(bench2_summary) +
   theme_bw() +
-  aes(x = subjects, 
+  aes(x = encounters,
       ymin = q1,
       y = median,
       ymax = q3,
@@ -43,11 +43,14 @@ ggplot(bench1_summary) +
   annotation_logticks() +
   xlab("Encounters") +
   ylab("Time (seconds)") +
-  facet_wrap(. ~ fifelse(subconditions, paste(method, "(with subconditions)"), method)) +
+  facet_wrap(
+    . ~ fifelse(subconditions, paste(method, "(with subconditions)"), method) + flag.method,
+    nrow = 2
+    ) +
   theme(
     panel.grid.minor = element_blank(),
     legend.position = "bottom",
     axis.text.x = element_text(hjust = 0.75)
   )
 
-ggsave(file = "bench1.svg", width = 7, height = 7)
+ggsave(file = "benchmark2.svg", width = 7, height = 4)
---
+++ benchmarking/make_grid1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DATA_CLASSES=(DF DT TBL)
+SUBJECTS=(1e1 1e2 1e3 5e3) # 1e4 5e4 1e5 5e5 1e6)
+METHODS=(pccc_v3.1 pccc_v3.1s charlson_quan2005 elixhauser_quan2005)
+SEEDS=$(seq 1 10)
+ITERS=$(seq 1 10)
+
+{
+  echo -e "data_class\tsubjects\tmethod\tseed\titer"
+  for dc in "${DATA_CLASSES[@]}"; do
+    for n in "${SUBJECTS[@]}"; do
+      for m in "${METHODS[@]}"; do
+        for s in ${SEEDS[@]}; do
+          for i in ${ITERS}; do
+            echo -e "${dc}\t${n}\t${m}\t${s}\t${i}"
+          done
+        done
+      done
+    done
+  done
+} > grid1.tsv
