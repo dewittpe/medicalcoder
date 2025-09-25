b06d3209c951fe1ea17a759e34b29d0aa5a8d51f
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-09-24 20:26:49 -0600

    update testing workflow

--- testing/build_r_from_source.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$1
-
-tar -xvf R-$VERSION.tar.gz
-export CURL_CONFIG="$PWD/curl8_spoof.sh"
-cd R-$VERSION
-./configure --with-blas --with-lapack
-make -j"$(nproc)"
--- testing/download_R_source.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-VERSION=$1
-MAJOR=`echo $VERSION | cut -c1-1`
-
-wget -N https://cran.r-project.org/src/base/R-$MAJOR/R-$VERSION.tar.gz
