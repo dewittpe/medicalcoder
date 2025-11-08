fc6f3903b3922fce8807fca6f43fa956ea4b1d3c
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-11-08 10:18:57 -0700

    refactor elixhauser_ahra_icd10.R to include the poa exempt codes

--- data-raw/elixhauser/elixhauser_ahrq_icd10.R
+++ data-raw/elixhauser/elixhauser_ahrq_icd10.R
@@ -5,25 +5,34 @@
 #          flags, POA rules, and index weights.
 #
 # inputs:
-#   ahrq/CMR_v2022-1.zip, CMR_v2023-1.zip, CMR_v2024-1.zip, CMR_v2025.1.zip
-#   ../icd/icd_codes.rds
+#   ahrq/CMR_v2022-1.zip
+#   ahrq/CMR_v2023-1.zip
+#   ahrq/CMR_v2024-1.zip
+#   ahrq/CMR_v2025.1.zip
 #
-# output: elixhauser_index_scores_ahrq_icd10.rds, elixhauser_poa_ahrq_icd10.rds,
-#         elixhauser_codes_ahrq_icd10.rds
+# output:
+#
+#   elixhauser_index_scores_ahrq_icd10.rds:
+#     a data.frame with the readmission/mortality index score for each condition
+#     by ahrqYYYY
+#
+#   elixhauser_codes_ahrq_icd10.rds:
+#     a data.frame mapping ICD-10 codes to conditions with 0/1 indicators for
+#     each ahrqYYYY
+#
+#   elixhauser_poa_ahrq_icd10.rds:
+#     a data.frame with the ahrqYYYY columns indicating if the condition is
+#     requires POA or not
 #
 # deps: data.table, readxl
 #
 # notes:
 #   Unzips AHRQ releases into tempdir(), parses SAS programs and reference
-#     workbooks, and normalises condition labels.
+#   workbooks
 #
 # idempotent: yes (deterministic once source archives are fixed)
 ################################################################################
-
 library(data.table)
-library(readxl)
-icd_codes <- readRDS("../icd/icd_codes.rds")
-setDT(icd_codes)
 
 ################################################################################
 # unzip the elixhauser source files into a temp directory
@@ -41,21 +50,18 @@ unzip("ahrq/CMR_v2023-1.zip", exdir = tmpdir)
 unzip("ahrq/CMR_v2024-1.zip", exdir = tmpdir)
 unzip("ahrq/CMR_v2025.1.zip", exdir = tmpdir, junkpaths = TRUE)
 
-# NOTE: AHRQ methods based on ICD-9 codes also require the use DRG codes.  I'm
-# not building that.  This is consistent with Quan (2005)
-#
-# From Quan (2005)
-#
-#> We did not employ the Diagnosis Related Group (DRG) screen option described
-#> by Elixhauser [1998] because the objective of this work was to develop
-#> algorithms to define comorbidities in undifferentiated hospital discharge
-#> data, and to then directly assess how well comorbidities derived from those
-#> algorithms predict mortality.
-#
-
 ################################################################################
-# Let's find all the labels for the comorbidities
-comorbidities <-
+# import all the SAS Programs for the ICD-10 version of Elixhauser
+
+format_programs <-
+  list(
+    "ahrq2022" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2022-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
+    "ahrq2023" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2023-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
+    "ahrq2024" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2024-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
+    "ahrq2025" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2025-1.sas"), what = character(), sep = "\n", quiet = !interactive())
+  )
+
+mapping_programs <-
   list(
     "ahrq2022" = scan(file = paste0(tmpdir, "/CMR_Mapping_Program_v2022-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
     "ahrq2023" = scan(file = paste0(tmpdir, "/CMR_Mapping_Program_v2023-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
@@ -63,9 +69,127 @@ comorbidities <-
     "ahrq2025" = scan(file = paste0(tmpdir, "/CMR_Mapping_Program_v2025-1.sas"), what = character(), sep = "\n", quiet = !interactive())
   )
 
-# find the lines between a LABEL and ;
-LABELSTART <- lapply(comorbidities, grep, pattern = "LABEL")
-SEMICOLON  <- lapply(comorbidities, grep, pattern = ";")
+index_programs <-
+  list(
+    "ahrq2022" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2022-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
+    "ahrq2023" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2023-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
+    "ahrq2024" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2024-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
+    "ahrq2025" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2025-1.sas"), what = character(), sep = "\n", quiet = !interactive())
+  )
+
+################################################################################
+# extract the sas code and build the needed R variables for conditions, ICD
+# codes, and POA exempt codes
+get_value_start_stop <- function(fp) {
+  values <- grep(fp, pattern = "Value \\$")
+  start  <- values + 1
+  stop   <- c(values[-1] - 1, length(fp))
+  values <- sub("(^.+\\$)(.+\\S)\\s*$", "\\2", fp[values])
+  strings <- vector(mode = "list", length = length(values))
+  for ( i in seq_len(length(values)) ) {
+    strings[[i]] <- fp[start[i]:stop[i]]
+  }
+  data.table::data.table(value = values, start, stop, strings)
+}
+
+values <-
+  lapply(format_programs, get_value_start_stop) |>
+  rbindlist(idcol = "version")
+
+build_comfmt <-
+  function(x) {
+    x <- sub(pattern = '=', replacement = "->", x)
+    x <- sub(pattern = '-> \\"(.+)\\"', replacement = ") -> \\1;", x = x)
+    strt <- grep("->", x) + 1
+    x[1] <- paste("c(", x[1])
+    x[strt] <- paste("c(", x[strt])
+    other_idx <- grep("other", x)
+    x <- x[-seq(other_idx, length(x))]
+    tmpfile <- tempfile()
+    cat(x, file = tmpfile)
+    e <- new.env()
+    source(tmpfile, local = e)
+    as.list(e)
+  }
+
+build_poaexmpt <- function(x) {
+  other_idx <- grep("other", x)
+  x <- x[-seq(other_idx, length(x))]
+  x <- sub('= \\"1\\"', ')', x)
+  x <- trimws(x)
+  x[1] <- paste("poaexmpt <- c(", x[1])
+  x
+  tmpfile <- tempfile()
+  cat(x, file = tmpfile)
+  e <- new.env()
+  source(tmpfile, local = e)
+  as.list(e)
+}
+
+values[, obj := strings]
+
+for (i in seq_len(nrow(values))) {
+  if (values[["value"]][i] == "COMFMT") {
+    data.table::set(values, i = i, j = "obj", value = list(build_comfmt(x = values[["strings"]][[i]])))
+  } else {
+    data.table::set(values, i = i, j = "obj", value = list(build_poaexmpt(x = values[["strings"]][[i]])))
+  }
+}
+
+
+# build a data.table for the elixhauser_codes
+elixhauser_codes <-
+  setNames(
+    values[value == "COMFMT"][["obj"]],
+    values[value == "COMFMT"][["version"]]
+  )
+
+elixhauser_codes <-
+  lapply(elixhauser_codes,
+    function(x) {
+      rbindlist(
+        lapply(names(x), function(nm) { data.table(condition = nm, code = x[[nm]]) })
+      )
+    })
+
+elixhauser_codes <- rbindlist(elixhauser_codes, idcol = "version")
+elixhauser_codes[, dummy := 1L]
+
+elixhauser_codes <-
+  dcast(
+    data = elixhauser_codes,
+    formula = condition + code ~ version,
+    value.var = "dummy",
+    fill = 0L
+  )
+
+elixhauser_codes[, icdv := 10L]
+elixhauser_codes[, dx := 1L]
+
+# poa exempt
+elixhauser_poaexempt <- values[value != "COMFMT"]
+elixhauser_poaexempt[, strings := NULL]
+elixhauser_poaexempt[, start := NULL]
+elixhauser_poaexempt[, stop := NULL]
+elixhauser_poaexempt <-
+  split(elixhauser_poaexempt, by = c("version", "value")) |>
+    lapply(function(x) {
+      y <-
+        data.table(
+          version = x$version,
+          value   = x$value,
+          code    = x[["obj"]][[1]]
+          )
+      y
+    }) |>
+  rbindlist()
+
+elixhauser_poaexempt <- unique(elixhauser_poaexempt$code)
+
+################################################################################
+# find the LABELS for the conditions
+LABELSTART <- lapply(mapping_programs, grep, pattern = "LABEL")
+SEMICOLON  <- lapply(mapping_programs, grep, pattern = ";")
 LABELEND   <- LABELSTART
 
 for (j in seq_along(SEMICOLON)) {
@@ -84,7 +208,7 @@ LABELS <-
     }, x = LABELSTART, y = LABELEND)
 
 LABELS <-
-  Map(function(c,l) {c[l[1]:l[2]]}, c = comorbidities, l = LABELS) |>
+  Map(function(m, l) {m[l[1]:l[2]]}, m = mapping_programs, l = LABELS) |>
   lapply(gsub, pattern = "LABEL", replacement = "") |>
   lapply(gsub, pattern = ";", replacement = "") |>
   lapply(gsub, pattern = "'", replacement = "") |>
@@ -92,34 +216,22 @@ LABELS <-
   lapply(trimws) |>
   lapply(function(x) fread(text = x, sep = "=", header = FALSE)) |>
   rbindlist(idcol = "method")
-setnames(LABELS, old = c("V1", "V2"), new = c("comorbidity", "comorbidity_description"))
+setnames(LABELS, old = c("V1", "V2"), new = c("condition", "condition_description"))
 
-# clean up some labels and descriptions
-LABELS[comorbidity_description == "Deficiency Anemias",
-       comorbidity_description := "Deficiency anemias"]
-LABELS[comorbidity_description == "Coagulopthy",
-       comorbidity_description := "Deficiency anemias"]
+LABELS[, dummy := 1L]
 
-LABELS[, dummy:=1L]
-
-LABELS <-
-  dcast(LABELS,
-        comorbidity + comorbidity_description ~ method,
-        value.var = "dummy",
-        fill = 0L)
-
-################################################################################
-# Index Scores
-elixhauser_index_scores <-
-  list(
-    "ahrq2022" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2022-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
-    "ahrq2023" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2023-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
-    "ahrq2024" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2024-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
-    "ahrq2025" = scan(file = paste0(tmpdir, "/CMR_Index_Program_v2025-1.sas"), what = character(), sep = "\n", quiet = !interactive())
+elixhauser_conditions <-
+  dcast(
+    LABELS,
+    condition + condition_description ~ method,
+    value.var = "dummy",
+    fill = 0L
   )
 
+################################################################################
+# Index scores
 elixhauser_index_scores <-
-  elixhauser_index_scores |>
+  index_programs |>
   lapply(grep, pattern = "^\\s*(r|m)w\\w+.*\\d\\s;", value = TRUE) |>
   lapply(sub, pattern = ";", replacement = "") |>
   lapply(strsplit, split = "=") |>
@@ -128,16 +240,26 @@ elixhauser_index_scores <-
   lapply(as.data.table)
 
 for (i in seq_along(elixhauser_index_scores)) {
-  setnames(elixhauser_index_scores[[i]], old = c("V1", "V2"), new = c("condition", names(elixhauser_index_scores)[i]))
+  setnames(
+    elixhauser_index_scores[[i]],
+    old = c("V1", "V2"),
+    new = c("condition", names(elixhauser_index_scores)[i])
+  )
 }
 
-elixhauser_index_scores <- Reduce(function(x,y){merge(x, y, all = TRUE, by = "condition")}, x = elixhauser_index_scores)
+elixhauser_index_scores <-
+  Reduce(
+    function(x, y){ merge(x, y, all = TRUE, by = "condition")},
+    x = elixhauser_index_scores
+  )
+
 elixhauser_index_scores[, index := fifelse(grepl("^rw", condition), "readmission", "mortality")]
 elixhauser_index_scores[, condition := sub("^(m|r)w", "", condition)]
 
 for (j in grep("^ahrq\\d{4}", names(elixhauser_index_scores))) {
   set(elixhauser_index_scores, j = j, value = as.integer(elixhauser_index_scores[[j]]))
 }
+
 
 ################################################################################
 # POA
@@ -154,6 +276,7 @@ for (i in seq_along(elixhauser_poa)) {
   elixhauser_poa[[i]] <- subset(elixhauser_poa[[i]], condition != "End of Content")
   elixhauser_poa[[i]][, poa_required := as.integer(poa_required == "Yes")]
   set(elixhauser_poa[[i]], j = names(elixhauser_poa)[i], value = 1L)
+  set(elixhauser_poa[[i]], j = "desc", value = NULL)
 }
 
 if (interactive()) {
@@ -162,66 +285,11 @@ if (interactive()) {
 
 elixhauser_poa <-
   Reduce(function(x,y) {
-           merge(x, y, all = TRUE, by = c('condition', 'desc', 'poa_required'))
+           merge(x, y, all = TRUE, by = c('condition', 'poa_required'))
        },
        x = elixhauser_poa)
 
 elixhauser_poa[, condition := sub("CMR_", "", condition)]
-
-################################################################################
-# import the codes
-elixhauser_codes <-
-  list(readxl::read_xlsx(paste0(tmpdir, "/CMR-Reference-File-v2022-1.xlsx"), sheet = 3, skip = 1),
-       readxl::read_xlsx(paste0(tmpdir, "/CMR-Reference-File-v2023-1.xlsx"), sheet = 3, skip = 1),
-       readxl::read_xlsx(paste0(tmpdir, "/CMR-Reference-File-v2024-1.xlsx"), sheet = 3, skip = 1),
-       readxl::read_xlsx(paste0(tmpdir, "/CMR-Reference-File-v2025-1.xlsx"), sheet = 3, skip = 1))
-
-elixhauser_codes <- lapply(elixhauser_codes, setDT)
-
-elixhauser_codes <- lapply(elixhauser_codes, subset, `ICD-10-CM Diagnosis` != "End of Content")
-elixhauser_codes <- lapply(elixhauser_codes, setnames, old = "ICD-10-CM Diagnosis", new = "code")
-elixhauser_codes <- lapply(elixhauser_codes, set, j = "ICD-10-CM Code Description", value = NULL)
-elixhauser_codes <- lapply(elixhauser_codes, set, j = "# Comorbidities", value = NULL)
-
-elixhauser_codes <- lapply(elixhauser_codes, melt, id.vars = "code", variable.name = "condition", variable.factor = FALSE)
-elixhauser_codes <- lapply(elixhauser_codes, subset, value > 0)
-
-for (i in 1:length(elixhauser_codes)) {
-  setnames(elixhauser_codes[[i]], old = "value", new = paste0("ahrq", as.character(2021 + i)))
-}
-
-elixhauser_codes <-
-  Reduce(f = function(x, y) {
-           merge(x, y, all = TRUE, by = c("code", "condition"))
-       },
-       x = elixhauser_codes
-  )
-
-for (j in grep("^ahrq\\d{4}", names(elixhauser_codes), value = TRUE)) {
-  set(elixhauser_codes, j = j, value = nafill(elixhauser_codes[[j]], type = "const", fill = 0L))
-  set(elixhauser_codes, j = j, value = as.integer(elixhauser_codes[[j]]))
-}
-
-elixhauser_codes[, icdv := 10L]
-elixhauser_codes[, dx   :=  1L]
-
-elixhauser_codes <-
-  merge(x = elixhauser_codes,
-        y = icd_codes,
-        all.x = TRUE,
-        by = c("icdv", "dx", "code"))
-
-elixhauser_codes <- elixhauser_codes[, .SD, .SDcols = patterns("code_id|condition|ahrq")]
-
-################################################################################
-# Import codes from the SAS format files
-sas_format <-
-  list(
-    "ahrq22" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2022-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
-    "ahrq23" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2023-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
-    "ahrq24" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2024-1.sas"), what = character(), sep = "\n", quiet = !interactive()),
-    "ahrq25" = scan(file = paste0(tmpdir, "/CMR_Format_Program_v2025-1.sas"), what = character(), sep = "\n", quiet = !interactive())
-  )
 
 ################################################################################
 # save to disk
@@ -233,6 +301,10 @@ saveRDS(elixhauser_poa, "./elixhauser_poa_ahrq_icd10.rds")
 
 setDF(elixhauser_codes)
 saveRDS(elixhauser_codes, "./elixhauser_codes_ahrq_icd10.rds")
+
+# elixhauser_poaexempt is just a character vector
+stopifnot(is.vector(elixhauser_poaexempt), is.character(elixhauser_poaexempt))
+saveRDS(elixhauser_poaexempt, "./elixhauser_poaexempt_ahrq_icd10.rds")
 
 ################################################################################
 #                                 End of File                                  #
