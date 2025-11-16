a99170f8b2d08bb084de522d27a1ea1ac5abd68e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-11-16 08:41:17 -0700

    clean up old code that was remaining after refactor

--- R/elixhauser.R
+++ R/elixhauser.R
@@ -207,27 +207,14 @@
 }
 
 .elixhauser_pre2022 <- function(ccc, id.vars, iddf, cmrb, poa.var, primarydx.var, method) {
-  # omit the rows for conditions requiring POA when POA = 0
-  # Conditions flagged as POA-required are only kept when the source
-  # code was present on admission; everything else remains eligible
-  # regardless of POA status.
-  idx <- Vectorize(isTRUE)(..mdcr_internal_elixhauser_poa..[[method]] == 1 & ..mdcr_internal_elixhauser_poa..[["poa_required"]] == 1L)
-  if (length(idx)) {
-    poa <- mdcr_subset(..mdcr_internal_elixhauser_poa.., i = idx)
-
-    idx <- ( (ccc[["condition"]] %in% poa[["condition"]]) & (ccc[[poa.var]] == 1L | ccc[["poaexempt"]] == 1)) |
-           (!(ccc[["condition"]] %in% poa[["condition"]]))
-
-    ccc <- mdcr_subset(ccc, i = idx)
-  }
 
   # what are the relevent coniditions
   conditions <-
     unique(..mdcr_internal_elixhauser_codes..[["condition"]][which(..mdcr_internal_elixhauser_codes..[[method]] == 1L)])
 
   # build indicator matrix
-  X <- matrix(0L, nrow = nrow(iddf), ncol = length(conditions))
-  colnames(X) <- conditions
+  X <- matrix(0L, nrow = nrow(iddf), ncol = length(conditions) + 1L)
+  colnames(X) <- c(conditions, "HTN_C")
 
   key_iddf <- do.call(paste, c(iddf, sep = "\r"))
   key_ccc  <- do.call(paste, c(mdcr_select(ccc, id.vars), sep = "\r"))
@@ -238,51 +225,8 @@
     X[cbind(ri[keep], ci[keep])] <- 1L
   }
 
-  if (startsWith(x = method, prefix = "elixhauser_ahrq20") | startsWith(x = method, prefix = "elixhauser_ahrq_icd10")) {
-
-    X[X[, "DRUG_ABUSEPSYCHOSES"] == 1L,  "DRUG_ABUSE"] <- 1
-    X[X[, "HFHTN_CX"] == 1L,             "HTN_CX"]     <- 1
-    X[X[, "HTN_CXRENLFL_SEV"] == 1L,     "HTN_CX"]     <- 1
-    X[X[, "HFHTN_CXRENLFL_SEV"] == 1L,   "HTN_CX"]     <- 1
-    X[X[, "ALCOHOLLIVER_MLD"] == 1L,     "ALCOHOL"]    <- 1
-    X[X[, "VALVE_AUTOIMMUNE"] == 1L,     "AUTOIMMUNE"] <- 1
-
-    X[X[, "DRUG_ABUSEPSYCHOSES"] == 1L,  "PSYCHOSES"]  <- 1
-    X[X[, "HFHTN_CX"] == 1L,             "HF"]         <- 1
-    X[X[, "HTN_CXRENLFL_SEV"] == 1L,     "RENLFL_SEV"] <- 1
-    X[X[, "HFHTN_CXRENLFL_SEV"] == 1L,   "HF"]         <- 1
-    X[X[, "HFHTN_CXRENLFL_SEV"] == 1L,   "RENLFL_SEV"] <- 1
-
-    idx <- which(X[, "CBVD_SQLAPARALYSIS"] == 1L)
-    X[idx, "PARALYSIS"] <- 1
-    X[idx, "CBVD_SQLA"] <- 1
-
-    X[X[, "ALCOHOLLIVER_MLD"] == 1L,     "LIVER_MLD"] <- 1
-    X[X[, "VALVE_AUTOIMMUNE"] == 1L,     "VALVE"]     <- 1
-    #X[X[, "CBVD_POA"] == 1L,             "CBVD_NPOA"] <- 1
-
-    # exlcusions
-    X[X[, "DIAB_CX"] == 1, "DIAB_UNCX"] <- 0L
-    X[X[, "HTN_CX"] == 1, "HTN_UNCX"] <- 0L
-    mets <- which(X[, "CANCER_METS"] == 1L)
-    X[mets, "CANCER_SOLID"] <- 0L
-    X[mets, "CANCER_NSITU"] <- 0L
-    X[X[, "CANCER_SOLID"] == 1, "CANCER_NSITU"] <- 0L
-    X[X[, "LIVER_SEV"] == 1, "LIVER_MLD"] <- 0L
-    X[X[, "RENLFL_SEV"] == 1, "RENLFL_MOD"] <- 0L
-
-    idx <- which(
-      (X[, "CBVD_POA"] == 1L) |
-      #(X[, "CBVD_POA"] == 0L & X[, "CBVD_NPOA"] == 0L & X[, "CBVD_SQLA"] == 1L)
-      (X[, "CBVD_SQLA"] == 1L)
-    )
-    X <- cbind(X, "CBVD" = 0L)
-    X[idx, "CBVD"] <- 1L
-  } else {
-    X <- cbind(X, "HTN_C" = 0L)
-    X[X[, "HTN_UNCX"] == 1L | X[, "HTN_CX"] == 1L, "HTN_C"] <- 1L
-    X[X[, "DMCX"] == 1, "DM"] <- 0L
-  }
+  X[X[, "HTN_UNCX"] == 1L | X[, "HTN_CX"] == 1L, "HTN_C"] <- 1L
+  X[X[, "DMCX"] == 1, "DM"] <- 0L
 
 
   mortality_weights <-
