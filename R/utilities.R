dbb03143f51dbb6471db1d59cdca5958e7cf4413
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-12-02 15:14:39 -0700

    Revert "remove unneed data.table::copy() from medcr_select"
    This reverts commit 55ebd0ddc3c5e12e8745dbe0b776f617d958ab48.

--- R/utilities.R
+++ R/utilities.R
@@ -50,7 +50,7 @@ mdcr_select <- function(x, cols) {
   }
 
   if (requireNamespace("data.table", quietly = TRUE) && inherits(x, "data.table")) {
-    return(x[, cols, drop = FALSE, with = FALSE])
+    return(getExportedValue(name = "copy", ns = "data.table")(x[, cols, drop = FALSE, with = FALSE]))
   } else {
     return(x[, cols, drop = FALSE])
   }
